06fb1dd466a1b6ad94ca10347cdbaea8d2823a1d
Tyler Arciniaga <tylerarc@umich.edu>
2025-05-28 12:18:17 -0400

    chore: <export -s> integrated -s flag into export HTML mode

--- internal/export/exportHtml.go
+++ internal/export/exportHtml.go
@@ -6,15 +6,27 @@ import (
 	"text/template"
 	"time"
 
+	data "github.com/Tyler-Arciniaga/DevLog/internal/data"
 	debug "github.com/Tyler-Arciniaga/DevLog/internal/debug"
 	notes "github.com/Tyler-Arciniaga/DevLog/internal/note"
 )
 
-func ExportHTML(format string, timeStyle string) {
+func ExportHTML(format string, sinceDate string) {
+	var exportData data.ExportData
 	//extract both note and debug json files
-
 	currLog, _ := notes.GetCurrLog()
+	exportData.Notes = currLog
+
+	if sinceDate == "" {
+		exportData.StartDate = 0
+	} else {
+		noEarlier, err := time.Parse("2006-01-02", sinceDate)
+		checkErr(err)
+		exportData.StartDate = noEarlier.Unix()
+	}
+
 	debugLog, _, _ := debug.GetCurrDebugLog()
+	exportData.DebugList = debugLog
 
 	//create string builder object and temp string line objects
 	var newExport strings.Builder
@@ -25,7 +37,6 @@ func ExportHTML(format string, timeStyle string) {
 	newExport.WriteString(sLine)
 
 	//create func map for template to use in pipelining
-
 	funcMap := template.FuncMap{
 		"formatTimeStamp": formatTimeStamp,
 	}
@@ -34,8 +45,8 @@ func ExportHTML(format string, timeStyle string) {
 	newMD := template.New("newMD").Funcs(funcMap)
 
 	//loop through notes log and parse accordingly, then write into string builder
-	newMD = template.Must(newMD.Parse("{{range .}}<p1>- [{{.Timestamp | formatTimeStamp}}] {{.Title}} {{if .Tag -}}({{.Tag}}){{end}}<p1><br>{{end}}<br>"))
-	newMD.Execute(&newExport, currLog)
+	newMD = template.Must(newMD.Parse("{{range .Notes}}{{if gt .Timestamp $.StartDate}}<p1>- [{{.Timestamp | formatTimeStamp}}] {{.Title}} {{if .Tag -}}({{.Tag}}){{end}}<p1><br>{{end}}{{end}}<br>"))
+	newMD.Execute(&newExport, exportData)
 
 	//header of debug tasks
 	sLine = "<h2>🐛 Debug Tasks</h2>"
